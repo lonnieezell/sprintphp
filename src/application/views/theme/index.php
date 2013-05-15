3b63b290af92778aa0079fbf2098e779912cd4dd
Lonnie Ezell <lonnieje@gmail.com>
2013-05-14 23:59:11 -0500

    Added a simple theme layout.

---
+++ src/application/views/theme/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>SprintPHP</title>
+
+</head>
+<body>
+
+    <?= $view_content ?>
+
+    <footer>
+        <p>Page rendered in {elapsed_time} seconds, using {memory_usage}.</p>
+    </footer>
+</body>
+</html>
